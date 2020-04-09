e94252cddf93a581063eac2b90b0155b45eb56d9
Herick Portugues <herick.portugues@gmail.com>
2020-04-09 12:51:20 -0300

    Estruturando os dados dos produtos

--- lib/tabs/products_tab.dart
+++ lib/tabs/products_tab.dart
@@ -11,7 +11,6 @@ class ProductsTab extends StatelessWidget {
         if(!snapshot.hasData)
           return Center(child: CircularProgressIndicator());
         else {
-
           var dividedTiles = ListTile.divideTiles(
             tiles: snapshot.data.documents.map(
               (doc) {
