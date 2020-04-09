df8e70f9f24c9f75e4b27bf1a4b9e791207f9c0f
Herick Portugues <herick.portugues@gmail.com>
2020-04-08 23:24:49 -0300

    Criando um drawer customizado

---
+++ lib/widgets/custom_drawer.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CustomDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    Widget _buildDrawerBack() => Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 203, 236, 241),
+            Colors.white
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter
+        )
+      ),
+    );
+
+    return Drawer(
+      child: Stack(
+        children: <Widget>[
+          _buildDrawerBack(),
+          ListView(
+            padding: EdgeInsets.only(left: 32.0, top: 16.0),
+            children: <Widget>[
+              
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
